6eca29c71d60a990352e49720aeccb4157b97457
Oliver Speir <oliverspeir9@gmail.com>
2023-02-21 12:23:24 -0700

    conditional-callback-session

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,27 +10,27 @@ export default NextAuth({
             clientSecret: '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',
         }),
     ],
+    session: {
+        strategy: "jwt",
+      },
     callbacks: {
         async jwt(token, user, account, profile, isNewUser) {
-            var user_token = token.token.account
             return token;
         },
-        async session({ session, token, user }) {
-            user_credential = {
-                "provider": token.token.account.provider,
-            }
-            if (token.token.account.access_token) {
-                user_credential["auth_token"] = token.token.account.access_token
-            }
-            if (token.token.account.id_token) {
-                user_credential["auth_token"] = token.token.account.id_token
+        async session({ token }) {
+            if(token){
+                user_credential = {
+                    "provider": token.token.account.provider,
+                }
+                if (token.token.account.access_token) {
+                    user_credential["auth_token"] = token.token.account.access_token
+                }
+                if (token.token.account.id_token) {
+                    user_credential["auth_token"] = token.token.account.id_token
+                }
             }
             return user_credential
         }
     },
-    // secret: "G98di5p1KYGycZRa9wOhULNe0uwEv9JwPOv1Nw+wWZI=",
-    // jwt: {
-    //     secret: "G98di5p1KYGycZRa9wOhULNe0uwEv9JwPOv1Nw+wWZI=",
-    //     encryption: true,
-    // },
+    secret: "G98di5p1KYGycZRa9wOhULNe0uwEv9JwPOv1Nw+wWZI=",
 });
