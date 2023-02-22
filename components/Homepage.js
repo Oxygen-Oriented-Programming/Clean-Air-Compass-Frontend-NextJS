7da9df90b249006c19f5beb30c2e0682a8ea6c37
Dennis Nichols <dgnichols22@gmail.com>
2023-02-21 23:06:11 -0600

    hotfix of merge conflicts

--- components/Homepage.js
+++ components/Homepage.js
@@ -8,7 +8,6 @@ import RightSidebar from "./RightSidebar";
 export default function Homepage() {
   const [showRightSidebar, setShowRightSidebar] = useState(false);
 
-<<<<<<< Updated upstream
     const [locationName, setLocationName] = useState('');
     const [locationData, setLocationData] = useState('');
     const [loading, setLoading] = useState(false);
@@ -42,54 +41,9 @@ export default function Homepage() {
         } else {
             alert("This is not a valid city name or zip code");
         }
-=======
-  const [locationName, setLocationName] = useState("");
-  const [locationData, setLocationData] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  function handleLocationInput(e) {
-    setLocationName(e.target.value);
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    const zipRegex = /^\d{5}(-\d{4})?$/;
-    const cityRegex = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
-    if (zipRegex.test(locationName) || cityRegex.test(locationName)) {
-      setLoading(true);
-      let baseUrl = "https://dolphin-app-ebj76.ondigitalocean.app/points/";
-      let url = baseUrl + locationName;
-      console.log(url);
-      try {
-        const response = await fetch(url);
-        const apiData = await response.json();
-        setLocationData(apiData);
-        console.log(response);
-        console.log(apiData);
-        console.log(locationData);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-        alert("An error occurred while fetching data from the API");
-      }
-    } else {
-      alert("This is not a valid city name or zip code");
->>>>>>> Stashed changes
     }
-  }
+  
 
-<<<<<<< Updated upstream
-    return (
-        <>
-            <div className='relative min-h-screen md:flex '>
-                <Sidebar handleLocationInput={handleLocationInput} handleSubmit={handleSubmit} loading={loading} />
-                <div className='flex-1 '>
-                    <Map locationData={locationData} setMap={setMap}/>
-                </div>
-            </div>
-        </>
-    );
-=======
   return (
     <>
       <div className="relative min-h-screen md:flex ">
@@ -98,7 +52,7 @@ export default function Homepage() {
           handleSubmit={handleSubmit}
           loading={loading}
         />
-        <Map className="" locationData={locationData} />
+        <Map className="" locationData={locationData} setMap = {setMap}/>
         {showRightSidebar && (
           <RightSidebar
             sidebar_show={showRightSidebar}
@@ -115,5 +69,4 @@ export default function Homepage() {
       </div>
     </>
   );
->>>>>>> Stashed changes
 }
