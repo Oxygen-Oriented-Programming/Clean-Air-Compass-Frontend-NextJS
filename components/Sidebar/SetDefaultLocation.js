d1b046347e4fbc36c193f27d325d993d6b9c5c0d
Oliver Speir <oliverspeir9@gmail.com>
2023-02-22 16:37:52 -0700

    acp

--- components/Sidebar/SetDefaultLocation.js
+++ components/Sidebar/SetDefaultLocation.js
@@ -1,4 +1,4 @@
-export default function SetDefaultLocation({ auth_token, user_id }) {
+export default function SetDefaultLocation({ auth_token, user_id, default_location }) {
   async function backendapi_setLocation(e) {
     e.preventDefault();
     const location = e.target.default_location.value;
@@ -19,7 +19,7 @@ export default function SetDefaultLocation({ auth_token, user_id }) {
       })
       .catch((error) => {
         console.log('catching');
-        fetch(`http://127.0.0.1:8000/default_locations/${user_number}/`, {
+        fetch(`http://127.0.0.1:8000/default_locations/${default_location}/`, {
           method: 'put',
           headers: {
             'Content-Type': 'application/json',
