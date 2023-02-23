d013529f8aea918b89f2e302715c30c0d953fc6b
Jason Christopher <jchristopher2448@gmail.com>
2023-02-22 23:22:06 -0600

    default locations now render. Can update default location on front end.

--- components/Chart.js
+++ components/Chart.js
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import faqChart from 'public/faqChart.png';
 
 export default function Chart(props) {
-  console.log(props);
   return (
     <>
       <div className='absolute flex flex-row items-center justify-around right-56 top-5 md:block sm:block'>
--- components/Homepage.js
+++ components/Homepage.js
@@ -7,96 +7,90 @@ import Chart from './Chart';
 import AlertMessage from './AlertMessage';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-// console.log(BASE_URL);
 
 export default function Homepage({ BASE_URL }) {
-  const [showRightSidebar, setShowRightSidebar] = useState(false);
+    const [showRightSidebar, setShowRightSidebar] = useState(false);
+    const [locationName, setLocationName] = useState('');
+    const [locationData, setLocationData] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [map, setMap] = useState(null);
+    const [message, setMessage] = useState('');
+    const baseUrl = BASE_URL;
 
-  const [locationName, setLocationName] = useState('');
-  const [locationData, setLocationData] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [map, setMap] = useState(null);
-  const [message, setMessage] = useState('');
-  const baseUrl = BASE_URL;
-
-  function handleLocationInput(e) {
-    setLocationName(e.target.value);
-  }
-
-  function fly_animation(apiData) {
-    map.flyTo([apiData.center_point[1], apiData.center_point[0]], 12, {
-      animate: true,
-      duration: 5,
-    });
-  }
-
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    console.log(baseUrl);
-
-    const zipRegex = /^\d{5}(-\d{4})?$/;
-    const cityRegex = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
-    if (zipRegex.test(locationName) || cityRegex.test(locationName)) {
-      setLoading(true);
-      let path = baseUrl;
-      let url = path + locationName;
-      try {
-        const response = await fetch(url);
-        const apiData = await response.json();
-        if (apiData.hasOwnProperty('message')) {
-          setMessage(apiData.message);
-          setLoading(false);
-          return;
-        }
-        setLoading(false);
-        setMessage('');
-        fly_animation(apiData);
-        setTimeout(() => {
-          setLocationData(apiData);
-        }, 5100);
-      } catch (error) {
-        console.error(error);
-        alert('An error occurred while fetching data from the API');
-      }
-    } else {
-      alert('This is not a valid city name or zip code');
+    function handleLocationInput(e) {
+        setLocationName(e.target.value);
     }
-  }
 
-  return (
-    <>
-      <div className='relative min-h-screen md:flex '>
-        <Sidebar
-          handleLocationInput={handleLocationInput}
-          handleSubmit={handleSubmit}
-          loading={loading}
-        />
-        {message && <AlertMessage message={message} />}
-        <Map
-          className=''
-          locationData={locationData}
-          setMap={setMap}
-          map={map}
-        />
-        <Chart className='' />
-        <div className=''>
-          {showRightSidebar && (
-            <RightSidebar
-              sidebar_show={showRightSidebar}
-              set_show={setShowRightSidebar}
-            />
-          )}
-          {!showRightSidebar ? (
-            <RightSidebarButton
-              sidebar_show={showRightSidebar}
-              set_show={setShowRightSidebar}
-              text='More Info'
-            />
-          ) : null}
-        </div>
-      </div>
-    </>
-  );
+    function fly_animation(apiData) {
+        map.flyTo([apiData.center_point[1], apiData.center_point[0]], 12, {
+            animate: true,
+            duration: 5,
+        });
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const zipRegex = /^\d{5}(-\d{4})?$/;
+        const cityRegex = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
+        if (zipRegex.test(locationName) || cityRegex.test(locationName)) {
+            setLoading(true);
+            let path = baseUrl;
+            let url = path + locationName;
+            try {
+                const response = await fetch(url);
+                const apiData = await response.json();
+                if (apiData.hasOwnProperty('message')) {
+                    setMessage(apiData.message);
+                    setLoading(false);
+                    return;
+                }
+                setLoading(false);
+                setMessage('');
+                fly_animation(apiData);
+                setTimeout(() => {
+                    setLocationData(apiData);
+                }, 5100);
+            } catch (error) {
+                console.error(error);
+                alert('An error occurred while fetching data from the API');
+            }
+        } else {
+            alert('This is not a valid city name or zip code');
+        }
+    }
+
+    return (
+        <>
+            <div className='relative min-h-screen md:flex '>
+                <Sidebar
+                    handleLocationInput={handleLocationInput}
+                    handleSubmit={handleSubmit}
+                    loading={loading}
+                />
+                {message && <AlertMessage message={message} />}
+                <Map
+                    className=''
+                    locationData={locationData}
+                    setMap={setMap}
+                    map={map}
+                />
+                <Chart className='' />
+                <div className=''>
+                    {showRightSidebar && (
+                        <RightSidebar
+                            sidebar_show={showRightSidebar}
+                            set_show={setShowRightSidebar}
+                        />
+                    )}
+                    {!showRightSidebar ? (
+                        <RightSidebarButton
+                            sidebar_show={showRightSidebar}
+                            set_show={setShowRightSidebar}
+                            text='More Info'
+                        />
+                    ) : null}
+                </div>
+            </div>
+        </>
+    );
 }
