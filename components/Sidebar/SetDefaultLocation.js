dddb539f3ce83bd314a93343b1b818abe6f66637
DeShon Dixon <deshondixon@gmail.com>
2023-02-22 19:17:55 -0800

    fixedforms

--- components/Sidebar/SetDefaultLocation.js
+++ components/Sidebar/SetDefaultLocation.js
@@ -1,4 +1,8 @@
-export default function SetDefaultLocation({ auth_token, user_id, default_location }) {
+export default function SetDefaultLocation({
+  auth_token,
+  user_id,
+  default_location,
+}) {
   async function backendapi_setLocation(e) {
     e.preventDefault();
     const location = e.target.default_location.value;
@@ -19,43 +23,51 @@ export default function SetDefaultLocation({ auth_token, user_id, default_locati
       })
       .catch((error) => {
         console.log('catching');
-        fetch(`${process.env.NEXT_PUBLIC_DEFAULT_LOCATION_BASE_URL}${user_number}/`, {
-          method: 'put',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            user: user_number,
-            default_location: location,
-          }),
-        }).then((response) => response.json());
+        fetch(
+          `${process.env.NEXT_PUBLIC_DEFAULT_LOCATION_BASE_URL}${user_number}/`,
+          {
+            method: 'put',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              user: user_number,
+              default_location: location,
+            }),
+          }
+        ).then((response) => response.json());
       });
   }
   return (
     <>
-      <form onSubmit={backendapi_setLocation}>
-        <div>
-          <label
-            htmlFor='first_name'
-            className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-          >
-            Default Location
-          </label>
-          <input
-            type='text'
-            id='default_location'
-            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-            placeholder='Seattle'
-            required
-          ></input>
-        </div>
-        <button
-          type='submit'
-          className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+      <div className='flex flex-col items-center w-full space-y-2.5'>
+        <form
+          onSubmit={backendapi_setLocation}
+          className='items-center p-3 px-4 mt-3 text-center duration-300 bg-gray-700 rounded-md cursor-pointer w-fit'
         >
-          Submit
-        </button>
-      </form>
+          <div>
+            <label
+              htmlFor='first_name'
+              className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+            >
+              Default Location
+            </label>
+            <input
+              type='text'
+              id='default_location'
+              className='h-10 text-lg font-normal text-center bg-transparent rounded-lg'
+              placeholder='Seattle'
+              required
+            ></input>
+          </div>
+          <button
+            type='submit'
+            className='px-4 py-2 m-auto font-semibold text-blue-400 bg-transparent border border-blue-500 rounded hover:bg-blue-500 hover:text-white hover:border-transparent'
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </>
   );
 }
