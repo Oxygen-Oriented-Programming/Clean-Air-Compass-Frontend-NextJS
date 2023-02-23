6d2dc4e2071b68a869964aa3cbaf73306e913239
Jason Christopher <jchristopher2448@gmail.com>
2023-02-23 14:30:04 -0600

    added modal and removed user geo location data

---
+++ components/AlertModal.js
@@ -0,0 +1,43 @@
+import Modal from 'react-modal';
+Modal.setAppElement('#__next');
+
+export default function AlertModal(props) {
+  return (
+    <div>
+      <Modal
+        isOpen={props.isModalOpen}
+        onRequestClose={props.toggleModal}
+        contentLabel="Example Modal"
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <h1 className="text-2xl font-bold mb-4">Modal Title</h1>
+        <p className="mb-4">Modal content goes here.</p>
+        <button onClick={props.toggleModal}>Close Modal</button>
+      </Modal>
+
+      <style jsx global>{`
+        .modal {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          background-color: white;
+          padding: 2rem;
+          border-radius: 0.5rem;
+          width: 400px;
+          max-width: 100%;
+        }
+        .overlay {
+          position: fixed;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          background-color: rgba(0, 0, 0, 0.5);
+          z-index: 9999;
+        }
+      `}</style>
+    </div>
+  );
+}
--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -12,6 +12,7 @@ export default function Sidebar(props) {
           handleSubmit={props.handleSubmit}
           loading={props.loading}
           text='X'
+          toggleModal={props.toggleModal}
         />
       </div>
     </>
