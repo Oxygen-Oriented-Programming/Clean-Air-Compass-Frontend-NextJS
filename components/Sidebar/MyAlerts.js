9587b8ed27ad355d6df746f728cd122bed6b8e25
DeShon Dixon <deshondixon@gmail.com>
2023-02-23 23:11:38 -0800

    new modal

--- components/Sidebar/MyAlerts.js
+++ components/Sidebar/MyAlerts.js
@@ -57,7 +57,7 @@ export default function MyAlerts() {
   return (
     <>
       <div className='flex flex-col transition-all items-center w-full h-full space-y-2.5 bg-transparent pb-2'>
-        <h3 className='my-4 text-lg font-bold text-center text-white '>
+        <h3 className='my-4 font-mono text-3xl font-bold text-center text-white '>
           My Alerts
         </h3>
         <form
@@ -65,10 +65,10 @@ export default function MyAlerts() {
           className='flex flex-col items-center justify-center w-full p-2 mx-auto my-4 text-white rounded-xl md:w-2/3 '
           ref={formRef}
         >
-          <div className='flex flex-col items-center justify-center w-full px-2 round-4xl sm:w-1/2 md:w-3/5 sm:px-4'>
+          <div className='flex flex-col items-center justify-center px-2 round-4xl sm:w-1/2 md:w-3/5 sm:px-4'>
             <label
               htmlFor='location'
-              className='flex pt-3 text-2xl font-bold text-center'
+              className='flex pt-3 font-mono text-2xl font-bold text-center'
             >
               Location
             </label>
@@ -76,14 +76,14 @@ export default function MyAlerts() {
               name='location'
               id='location'
               type='text'
-              className='h-10 text-lg font-normal text-center transition-all bg-gray-800 rounded-lg'
+              className='h-16 text-lg font-normal text-center bg-gray-800 '
               placeholder='Enter a City'
             />
           </div>
           <div className='flex flex-col items-center justify-center w-full px-2 round-4xl sm:w-1/2 md:w-3/5 sm:px-4'>
             <label
               htmlFor='phoneNumber'
-              className='block mb-1 text-xl font-bold text-center '
+              className='block mb-1 font-mono text-xl font-bold text-center text-white'
             >
               Phone Number
             </label>
@@ -91,14 +91,14 @@ export default function MyAlerts() {
               name='phoneNumber'
               id='phoneNumber'
               type='text'
-              className='h-10 text-lg font-normal text-center transition-all bg-gray-800 rounded-lg'
+              className='h-16 text-lg font-normal text-center bg-gray-800'
               placeholder='(xxx) xxx-xxxx'
             />
           </div>
-          <div className='flex flex-col items-center p-4 pt-2 w-fit sm:w-1/2 sm:px-5 w-[20vw]'>
+          <div className='flex flex-col items-center w-32 p-4 pt-2 font-mono sm:w-1/2 sm:px-5'>
             <label
               htmlFor='airQualityThreshold'
-              className='block mb-1 text-lg font-bold text-center '
+              className='block mb-1 text-lg font-bold text-center font mono '
             >
               Air Quality
             </label>
@@ -132,7 +132,7 @@ export default function MyAlerts() {
           </div>
           <button
             type='submit'
-            className='px-4 py-2 font-bold text-white transition-all bg-transparent border-2 border-blue-900 rounded hover:bg-blue-900 hover:border-transparent hover:animate-none animate-pulse'
+            className='px-6 py-4 mt-4 mb-2 font-mono text-2xl text-white transition-all bg-transparent border border-purple-500 rounded hover:bg-purple-700 hover:border-transparent'
           >
             Create Alert
           </button>
@@ -144,7 +144,7 @@ export default function MyAlerts() {
             alerts.map((alert, i) => (
               <div
                 key={i}
-                className='flex items-center justify-between w-full px-2 mb-2 bg-gray-100 rounded-md sm:px-4'
+                className='flex items-center justify-between w-full px-2 mb-2 text-center text-black bg-gray-100 sm:px-4'
               >
                 <p className='text-sm'>
                   {alert.location} - {alert.air_quality_threshold}
