e1b8a5b55fadcfea0414f668b6b385f76207d88e
Oliver Speir <oliverspeir9@gmail.com>
2023-05-09 09:33:51 -0600

    acp

--- components/Sidebar/dist/SidebarButton.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-
-var _link = _interopRequireDefault(require("next/link"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
