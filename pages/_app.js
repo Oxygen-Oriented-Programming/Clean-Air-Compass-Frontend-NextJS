60dd44b8450387a04e7009d6c22e2282a9ef2606
Oliver Speir <oliverspeir9@gmail.com>
2023-05-09 05:11:30 -0600

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({
   Component,
@@ -8,6 +9,7 @@ export default function App({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 }
